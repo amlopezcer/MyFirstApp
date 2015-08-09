c6f7bdbf92aa9df5c7000f9ff690e5fbfadf8994
Alejandro M. Lopez Cerezo <am.lopezcer@gmail.com>
2015-08-09 10:54:46 +0200

    Añade comunicación entre Actividades
    Se crea una nueva actividad que recibe un inent de la primera; además queda incluída la flecha de navegación entre ambas.

--- app/src/main/java/com/amlopezc/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/amlopezc/myfirstapp/DisplayMessageActivity.java
@@ -5,17 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
-public class MyActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "com.amlopezc.myFirstApp.MESSAGE";
+public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //Set the text view as the activity layout
+        setContentView(textView);
     }
 
     @Override
@@ -31,14 +39,5 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /** Called when the user clicks the Send Button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
